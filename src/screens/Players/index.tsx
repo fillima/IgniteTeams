960375c165a866a1853f92601da85bdeeed48755
Filipe <filipelima849@gmail.com>
2023-05-25 17:48:36 -0300

    Criado componente filter e contagem de jogadores

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,11 +1,17 @@
 import { Header } from "@components/Header";
-import { Container, Form } from "./styles";
+import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 import { Button } from "@components/Button";
 import { Highlight } from "@components/Highlight";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Input } from "@components/Input";
+import { Filter } from "@components/Filter";
+import { FlatList } from "react-native";
+import { useState } from "react";
 
 export function Players() {
+    const [team, setTeam] = useState("Time A");
+    const [players, setPlayers] = useState([]);
+
     return(
         <Container>
             <Header showBackButton/>
@@ -20,6 +26,22 @@ export function Players() {
                 />
                 <ButtonIcon icon="add" />
             </Form>
+            <HeaderList>
+                <FlatList 
+                    data={["Time A", "Time B"]}
+                    keyExtractor={item => item}
+                    renderItem={({item}) => (
+                        <Filter
+                            title={item}
+                            isActive={item === team}
+                            onPress={() => setTeam(item)}
+                        />
+                    )}
+                    horizontal
+                />
+                <NumberOfPlayers>{players.length}</NumberOfPlayers>
+            </HeaderList>
+            
             <Button
                 title="Remover turma"
                 type="SECONDARY"
