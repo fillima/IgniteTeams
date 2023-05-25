e37a8adf45ea668d0b0081fdadf7527f0e249891
Filipe <filipelima849@gmail.com>
2023-05-25 14:57:13 -0300

    Adicionado estilização do placeholder

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -14,7 +14,9 @@ export function NewGroup() {
                     title="Nova Turma"
                     subtitle="Crie uma turma para adicionar pessoas"
                 />
-                <Input />
+                <Input
+                    placeholder="Nome da turma"
+                />
                 <Button
                     title="Criar"
                 />
