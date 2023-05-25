a6b8f5b6b0536d843fd9954a735207b75fbb3d21
Filipe <filipelima849@gmail.com>
2023-05-25 15:27:46 -0300

    Refatorado código css

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,15 +1,17 @@
 import { TextInput } from "react-native";
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 
 export const Container = styled(TextInput)`
+    ${({theme}) => css`
+        background-color: ${theme.COLORS.GRAY_700};
+        color: ${theme.COLORS.WHITE};
+        font-size: ${theme.FONT_SIZE.MD}px;
+        font-family: ${theme.FONT_FAMILY.REGULAR};
+    `}
     flex: 1;
     min-height: 56px;
     max-height: 56px;
-    background-color: ${({theme}) => theme.COLORS.GRAY_700};
-    color: ${({theme}) => theme.COLORS.WHITE};
     border-radius: 6px;
     padding: 16px;
     margin-bottom: 12px;
-    font-size: ${({theme}) => theme.FONT_SIZE.MD}px;
-    font-family: ${({theme}) => theme.FONT_FAMILY.REGULAR};
 `;
