5d87853bf336c96a834958ff4655d8c6541f49b7
Filipe <filipelima849@gmail.com>
2023-05-29 17:35:47 -0300

    Tratando erros de forma personalizada

--- src/storage/group/groupCreate.ts
+++ src/storage/group/groupCreate.ts
@@ -1,10 +1,17 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { GROUP_COLLECTION } from "@storage/storageConfig";
 import { groupsGetAll } from "./groupsGetAll";
+import { AppError } from "@utils/AppError";
 
 export async function groupCreate(newGroup: string) {
     try {
         const storedGroups = await groupsGetAll();
+
+        const groupAllReadyExists = storedGroups.includes(newGroup);
+
+        if (groupAllReadyExists) {
+            throw new AppError('Já existe uma turma cadastrada com esse nome');
+        }
 
         const storage = JSON.stringify([...storedGroups, newGroup]);
 
