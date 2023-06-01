b4fecffa8e04da7fa3b595ba442fe5e1934aaa45
Filipe <filipelima849@gmail.com>
2023-06-01 18:08:52 -0300

    Adicionando players num time

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { FlatList } from "react-native";
+import { Alert, FlatList } from "react-native";
 import { useRoute } from "@react-navigation/native";
 
 import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
@@ -12,17 +12,42 @@ import { Input } from "@components/Input";
 import { Filter } from "@components/Filter";
 import { PlayerCard } from "@components/PlayerCard";
 import { ListEmpty } from "@components/ListEmpty";
+import { AppError } from "@utils/AppError";
+import { playerAddByGroup } from "@storage/player/playerAddByGroup";
 
 type RouteParams = {
     group: string
 }
 
 export function Players() {
+    const [newPlayerName, setNewPlayerName] = useState('');
     const [team, setTeam] = useState("Time A");
     const [players, setPlayers] = useState(['Filipe', 'Anne', 'Gabi', 'Maria', 'Matheus', 'Laysla', 'Juninho', 'Yago']);
 
     const route = useRoute();
     const { group } = route.params as RouteParams;
+
+    async function handleAddPlayer() {
+        if (newPlayerName.trim().length === 0) {
+            return Alert.alert("Nova pessoa", "Informe o nome da pessoa para adicionar");
+        }
+
+        const newPlayer = {
+            name: newPlayerName,
+            team,
+        }
+
+        try {
+            await playerAddByGroup(newPlayer, group);
+        } catch (error) {
+            if (error instanceof AppError) {
+                Alert.alert("Nova pessoa", error.message);
+            } else {
+                console.log(error);
+                Alert.alert("Nova pessoa", "Não foi possível adicionar.");
+            }
+        }
+    }
 
     return(
         <Container>
@@ -33,6 +58,7 @@ export function Players() {
             />
             <Form>
                 <Input
+                    onChangeText={setNewPlayerName}
                     placeholder="Nome da pessoa"
                     autoCorrect={false}
                 />
